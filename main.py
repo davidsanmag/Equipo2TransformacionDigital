1e713f3415a8dd8750748fa9aa9778adc00cec6f
Esteban de la Maza <A01720548@tec.mx>
2023-03-07 11:18:17 -0600

    Added dummy fields

--- main.py
+++ main.py
@@ -3,3 +3,15 @@ import streamlit as st
 
 st.title("Equipo 2")
 st.subheader("Transformacion Digital")
+
+code = st.text_input("Código de Socio Formador", type="password")
+
+project_name = st.selectbox("Seleccione el proyecto", [
+                            "Proyecto 1", "Proyecto 2", "Proyecto 3"])
+
+student_id = st.text_input("Mátricula del alumno")
+
+submit = st.button("Agregar a Proyecto")
+
+if submit:
+    st.success("Alumno agregado al proyecto")
